3339914519c2a270fa4282bf8c1827af3f45c00a
drmadwill <nofelsalahov9@gmail.com>
2023-12-18 16:37:16 +0400

    init

---
+++ DrMadWill.EventBus.Base/Abstractions/IEventBusSubscriptionManager.cs
@@ -0,0 +1,34 @@
+using DrMadWill.EventBus.Base.Events;
+
+namespace DrMadWill.EventBus.Base.Abstractions;
+
+public interface IEventBusSubscriptionManager
+{
+    bool IsEmpty { get; }
+    
+    event EventHandler<string> OnEventRemoved;
+
+    void AddSubscription<T, TH>()
+        where T : IntegrationEvent
+        where TH : IIntegrationEventHandler<T>;
+    
+    void RemoveSubscription <T, TH>()
+        where T : IntegrationEvent
+        where TH : IIntegrationEventHandler<T>;
+
+    bool HasSubscriptionForEvent<T>() where T : IntegrationEvent;
+    
+    bool HasSubscriptionForEvent(string eventName);
+    
+    Type GetEventTypeByName(string eventName);
+    
+    void Clear();
+    
+    IEnumerable<SubscriptionInfo> GetHandlerForEvent<T>() where T : IntegrationEvent;
+    
+    IEnumerable<SubscriptionInfo> GetHandlerForEvent(string name);
+    
+    string GetEventKey<T>();
+
+
+}
---
+++ DrMadWill.EventBus.RabbitMQ/RabbitMQPersistentConnection.cs
@@ -0,0 +1,84 @@
+using System.Net.Sockets;
+using Polly;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+
+namespace DrMadWill.EventBus.RabbitMQ;
+
+public class RabbitMqPersistentConnection : IDisposable
+{
+    private IConnection _connection;
+    private readonly IConnectionFactory _connectionFactory;
+    private readonly int _tryCount;
+    private object lock_object = new object();
+    private bool isDisposed = false;
+    public RabbitMqPersistentConnection(IConnectionFactory connectionFactory,int tryCount = 5)
+    {
+        _connectionFactory = connectionFactory;
+        _tryCount = tryCount;
+    }
+
+    public bool IsConnection => _connection != null && _connection.IsOpen;
+
+    public IModel CreateModel()
+    {
+        return _connection.CreateModel();
+    }
+
+    public void Dispose()
+    {
+        isDisposed = true;
+        _connection.Dispose();
+    }
+
+    public bool TryConnect()
+    {
+
+        lock (lock_object)
+        {
+            var policy = Policy.Handle<SocketException>()
+                .Or<BrokerUnreachableException>()
+                .WaitAndRetry(_tryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    (ex, time) =>
+                    {
+                        Console.WriteLine("ex => Event Bus TryConnect In RabbitMQ =>  " + ex);
+                    });
+
+            policy.Execute(() =>
+            {
+                _connection = _connectionFactory.CreateConnection();
+            });
+
+            if (IsConnection)
+            {
+                _connection.ConnectionShutdown += Connection_ConnectionShutdown; 
+                _connection.CallbackException += Connection_CallbackException; 
+                _connection.ConnectionBlocked += Connection_ConnectionBlocked; 
+                
+                return true;
+            }
+
+            return false;
+
+        }
+    }
+
+    private void Connection_ConnectionBlocked(object? sender, ConnectionBlockedEventArgs e)
+    {
+        if(isDisposed) return;
+        TryConnect();
+    }
+
+    private void Connection_CallbackException(object? sender, CallbackExceptionEventArgs e)
+    {
+        if(isDisposed) return;
+        TryConnect();
+    }
+
+    private void Connection_ConnectionShutdown(object? sender, ShutdownEventArgs e)
+    {
+        if(isDisposed) return;
+        TryConnect();
+    }
+}
